af4e2859ca6b08d60ca2b391bbd4ec0d5018dabf
viktormokrecov <viktormokrecov@gmail.com>
2020-02-29 13:01:25 +0300

    init commit

---
+++ src/main/java/com/vmokrecov/worldservice/WorldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vmokrecov.worldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vmokrecov/worldservice/WorldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmokrecov.worldservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
